9e05366a3eb91773d935deb1fc56016672d88bab
00258219 <00258219@uca.edu.sv>
2020-04-03 18:00:18 -0600

    Updates

---
+++ precache-manifest.a2ba2f54baf0866ed8891d4e0261ac84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8c187395328af3bac18c6aa3e42057",
+    "url": "/senkar/index.html"
+  },
+  {
+    "revision": "6323aa989c3f9838437e",
+    "url": "/senkar/static/css/main.0360288f.chunk.css"
+  },
+  {
+    "revision": "fd3ff744b2dcbaebdc9b",
+    "url": "/senkar/static/js/2.41288ba1.chunk.js"
+  },
+  {
+    "revision": "6323aa989c3f9838437e",
+    "url": "/senkar/static/js/main.ef653043.chunk.js"
+  },
+  {
+    "revision": "2af150a613a04f21fd07",
+    "url": "/senkar/static/js/runtime~main.70ea8463.js"
+  }
+]);
